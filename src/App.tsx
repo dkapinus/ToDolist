361a8d1815155248c00d0d74d1909eda2ceebe1b
dkapinus <dimonio@tut.by>
2022-11-15 23:09:03 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from './Todolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [ tasks1, setTask] =useState([
+            { id: 1, title: "HTML&CSS", isDone: true },
+            { id: 2, title: "JS", isDone: true },
+            { id: 3, title: "ReactJS", isDone: false }
+        ]
+    )
+    const removeTsk = (id:number)=> {
+        tasks1=tasks1.filter((el)=>el.id!==id)
+        setTask(tasks1)
+    }
+
+
+let [nameButton,setButton]=useState('All')
+
+    if(nameButton==='Active'){ tasks1=tasks1.filter((el)=>el.isDone===true)}
+    if(nameButton==='Completed'){ tasks1=tasks1.filter((el)=>el.isDone===false)}
+
+
+    const  callbackButton = (nameButton:string)=> {
+
+       setButton(nameButton)
+
+    }
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1} callback={removeTsk} callbackButton={callbackButton} />
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    callback:(id:number)=>void;
+    callbackButton:(nameButton:string)=>void
+}
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export function Todolist(props: PropsType) {
+
+    const onClickHandler =(nameButton:string) =>{
+        props.callbackButton(nameButton)
+    }
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+
+        <ul> {props.tasks.map((el) => {
+            return (
+                <div key={el.id}>
+                    <li>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                        <button onClick={(callback) => props.callback(el.id)}>X</button>
+                    </li>
+
+                </div>
+            )
+        })
+
+
+        }</ul>
+        <div>
+            <button onClick={()=>onClickHandler('All')}>All</button>
+            <button onClick={()=>onClickHandler('Active')}>Active</button>
+            <button onClick={()=>onClickHandler('Completed')}>Completed</button>
+        </div>
+    </div>
+}
