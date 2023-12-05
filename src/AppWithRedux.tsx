7679a90fa4807a12fef26a8612d19081647b02a6
dkapinus <dimonio@tut.by>
2023-12-05 19:33:14 +0300

    fixes update

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import {Login} from "./Components/Login/Login";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import {Todolists} from "./Todolists";
 import {SetLoginStatusTC} from "./Components/Login/auth-Reducer";
 import {AppRootStateType, useAppDispatch} from "./state/store";
@@ -12,22 +12,14 @@ import {useSelector} from "react-redux";
 function AppWithReducer() {
 
 
+    const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized)
 
-
-
-
-
-
-    const isInitialized =useSelector<AppRootStateType,boolean>(state => state.app.isInitialized)
-
-    const dispatch =useAppDispatch()
+    const dispatch = useAppDispatch()
 
     useEffect(() => {
 
         dispatch(SetLoginStatusTC())
     }, [dispatch])
-
-
 
 
     if (!isInitialized) {
@@ -41,14 +33,14 @@ function AppWithReducer() {
     return (
         <div>
 
-            <BrowserRouter>
-                                <Routes>
-                                    <Route path={"/"} element={<Todolists/>}/>
-                                    <Route path={"/login"} element={<Login/>}/>
-                                    <Route path={"/404"} element={<h1>{'404 Page not founded'}</h1>}/>
-                                    <Route path={'/*'} element={<Navigate to={"/404"}/>} />
-                                </Routes>
-                </BrowserRouter>
+            <HashRouter>
+                <Routes>
+                    <Route path={"/"} element={<Todolists/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/404"} element={<h1>{'404 Page not founded'}</h1>}/>
+                    <Route path={'/*'} element={<Navigate to={"/404"}/>}/>
+                </Routes>
+            </HashRouter>
 
 
         </div>
